315e94eada3748dec75e4cd905946abdf1e4b2b3
Melhefnawi <>
2023-05-30 11:28:26 +0000

    Insert the Welcome message

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("-------------------------------")
+print ("Welcome to Ultimate BATTLESHIPS!!\n""Board Size: 5 Number of ships: 4\n""Top left corner is row:0, col:0\n")
+print ("-------------------------------")
+
+user = input("please enter you name to start game:")
